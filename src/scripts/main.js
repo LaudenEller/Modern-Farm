1cf5f3327c6db32b1b97e0cef3711e391018b87a
Lauden Eller <laudlauden@gmail.com>
2022-01-24 09:44:00 -0800

    I was returning harvest inside the function too early, I fixed it and it passed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,40 @@
+// print welcome message
 console.log("Welcome to the main module")
 
-// import function from another seed
+// import function that generates a yearly plan
 import { createPlan } from "./plan.js"
 
-// store imported function to new variable
+// import function that populates field according to plan from tractor
+import { plantSeeds } from "./tractor.js"
+
+// import function that prints the "crops" in proper amounts
+import { harvestPlants } from "./harvester.js"
+
+// import function that returns a copy of the populated array
+import { usePlants } from "./field.js"
+
+// import function that prints harvested plants to a grid
+import { Catalog } from "./catalog.js"
+
+// save imported function to new variable
 const yearlyPlan = createPlan()
+
+// update the array in Field by calling the tractor function with the plan function passed as an argument
+plantSeeds(yearlyPlan)
+
+// save imported function to a new variable
+const populatedPlantArray = usePlants()
+
+// store imported function to new variable
+const harvestedPlantsArray = harvestPlants(populatedPlantArray)
+
+// populate index.HTML with HTML string
+const locationParentHTMLElement = document.querySelector(".container")
+
+locationParentHTMLElement.innerHTML = Catalog(harvestedPlantsArray)
+
+// print catalog of harvested plants
+console.log(Catalog)
 
 // print function to developer console
 // console.log(yearlyPlan)
@@ -40,21 +70,8 @@ const yearlyPlan = createPlan()
 // const wheatSeed = createWheat()
 // console.log(wheatSeed)
 
-import { plantSeeds } from "./tractor.js"
 
-plantSeeds(yearlyPlan)
 // console.log(plantSeedsVariable)
-
-import { usePlants } from "./field.js"
-
-
-const hereIsTheArray = usePlants()
-
-import { harvestPlants } from "./harvester.js"
-
-const harvestPlantsVar = harvestPlants(hereIsTheArray)
-
-console.log(harvestPlantsVar)
 
 // const seedObject = {};
 
